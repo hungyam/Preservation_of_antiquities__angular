e376a47e8b75f9e626ee10301aeaec52a17685f5
hungyam <1159292671@qq.com>
2021-03-03 00:57:24 +0800

    3.3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SigninComponent} from './signin/signin.component';
+import {RegistComponent} from './regist/regist.component';
+import {LogoutComponent} from './logout/logout.component';
+
 
 const routes: Routes = [
   {path: 'signin', component: SigninComponent},
+  {path: 'regist', component: RegistComponent},
+  {path: 'logout', component: LogoutComponent},
   {path: '', redirectTo: 'signin' , pathMatch : 'full'}
 ];
 
@@ -11,6 +16,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule { }
